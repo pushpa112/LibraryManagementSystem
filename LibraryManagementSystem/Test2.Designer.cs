2a0654df40fcb90ae052df14c41f702ae8c6bc22
Samrachhika <samie.saymi@gmail.com>
2020-11-18 22:46:28 -0600

    connection success

--- LibraryManagementSystem/Test2.Designer.cs
+++ LibraryManagementSystem/Test2.Designer.cs
@@ -29,32 +29,47 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(122, 92);
+            this.button1.Location = new System.Drawing.Point(163, 113);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(313, 169);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(46, 17);
+            this.messageLabel.TabIndex = 1;
+            this.messageLabel.Text = "label1";
+            // 
             // Test2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.messageLabel);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Test2";
             this.Text = "Form2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label messageLabel;
     }
 }
--- LibraryManagementSystem/Test2.cs
+++ LibraryManagementSystem/Test2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,23 @@ namespace LibraryManagementSystem
         private void button1_Click(object sender, EventArgs e)
         {
             // commenting here to test github
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Samrachhika\source\C#Project\frompushparough\LibraryManagementSystem\AppData\tryDB.mdf;Integrated Security=True;Connect Timeout=30");
+                string query = "Select * from userTB";
+                SqlDataAdapter sa = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                sa.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    messageLabel.Text = "Connection Successful ";
+                }
+            }
+            catch (Exception ex)
+            {
+                messageLabel.Text = ex.Message;
+
+            }
         }
     }
 }
