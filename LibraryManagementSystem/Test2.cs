0db04f623c00c72ea28a1af1a2ebb6e3a9efb339
pushpa112 <pushpa112@users.noreply.github.com>
2020-11-18 22:21:33 -0600

    added a buttoon

--- LibraryManagementSystem/Test2.cs
+++ LibraryManagementSystem/Test2.cs
@@ -16,5 +16,10 @@ namespace LibraryManagementSystem
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // commenting here to test github
+        }
     }
 }
